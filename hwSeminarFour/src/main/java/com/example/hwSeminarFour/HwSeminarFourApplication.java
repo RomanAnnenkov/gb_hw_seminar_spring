f39561afb424c23836c17372b5b829340a894d16
Roman Annenkov <roman.annenkov@gmail.com>
2024-03-21 12:10:24 +0300

    added project for hw from seminar four

---
+++ hwSeminarFour/src/main/java/com/example/hwSeminarFour/HwSeminarFourApplication.java
@@ -0,0 +1,13 @@
+package com.example.hwSeminarFour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSeminarFourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwSeminarFourApplication.class, args);
+	}
+
+}
---
+++ hwSeminarFour/src/test/java/com/example/hwSeminarFour/HwSeminarFourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwSeminarFour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSeminarFourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
