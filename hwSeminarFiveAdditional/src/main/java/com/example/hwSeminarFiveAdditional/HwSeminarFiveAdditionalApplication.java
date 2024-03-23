d67c1c78acd95ea77c8d3438ee82a8dbbb1933d0
Roman Annenkov <roman.annenkov@gmail.com>
2024-03-23 15:22:19 +0300

    added project for addditional task from seminar five

---
+++ hwSeminarFiveAdditional/src/main/java/com/example/hwSeminarFiveAdditional/HwSeminarFiveAdditionalApplication.java
@@ -0,0 +1,13 @@
+package com.example.hwSeminarFiveAdditional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSeminarFiveAdditionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwSeminarFiveAdditionalApplication.class, args);
+	}
+
+}
---
+++ hwSeminarFiveAdditional/src/test/java/com/example/hwSeminarFiveAdditional/HwSeminarFiveAdditionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwSeminarFiveAdditional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSeminarFiveAdditionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
