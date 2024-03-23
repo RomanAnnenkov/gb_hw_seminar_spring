b594a15fbf759ff28c3d6ab059dafec2c8d9c462
Roman Annenkov <roman.annenkov@gmail.com>
2024-03-23 11:42:27 +0300

    added project for hw from seminar five

---
+++ hwSeminarFive/src/main/java/com/example/hwSeminarFive/HwSeminarFiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.hwSeminarFive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSeminarFiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwSeminarFiveApplication.class, args);
+	}
+
+}
---
+++ hwSeminarFive/src/test/java/com/example/hwSeminarFive/HwSeminarFiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwSeminarFive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSeminarFiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
