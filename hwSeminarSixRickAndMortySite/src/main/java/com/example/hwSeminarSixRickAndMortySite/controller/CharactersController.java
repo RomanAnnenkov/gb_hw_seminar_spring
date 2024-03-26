f64e5bcdec5f08992409130fe64deaaf6fbc9a18
Roman Annenkov <roman.annenkov@gmail.com>
2024-03-26 22:28:56 +0300

    additinal task from seminar six hw added

--- hwSeminarSixRickAndMortySite/src/main/java/com/example/hwSeminarSixRickAndMortySite/controller/CharactersController.java
+++ hwSeminarSixRickAndMortySite/src/main/java/com/example/hwSeminarSixRickAndMortySite/controller/CharactersController.java
@@ -1,12 +1,13 @@
 package com.example.hwSeminarSixRickAndMortySite.controller;
 
 import com.example.hwSeminarSixRickAndMortySite.model.Characters;
+import com.example.hwSeminarSixRickAndMortySite.model.Result;
 import com.example.hwSeminarSixRickAndMortySite.service.CharactersService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @AllArgsConstructor
@@ -14,8 +15,16 @@ public class CharactersController {
 
     private CharactersService charactersService;
     @GetMapping("/")
-    public ResponseEntity<Characters> getCharactersPage() {
+    public String getCharactersPage(Model model) {
         Characters allCharacters = charactersService.getAllCharacters();
-        return new ResponseEntity<>(allCharacters, HttpStatus.OK);
+        model.addAttribute("results", allCharacters.getResults());
+        return "characters-list";
+    }
+
+    @GetMapping("/{id}")
+    public String getCharacterPage(@PathVariable int id, Model model) {
+        Result character = charactersService.getCharacterById(id);
+        model.addAttribute("result", character);
+        return "character";
     }
 }
