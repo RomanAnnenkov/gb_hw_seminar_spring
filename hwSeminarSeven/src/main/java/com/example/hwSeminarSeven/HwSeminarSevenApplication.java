c984cf1866bc90e652a81b79a8b1f1587bd7011f
Roman Annenkov <roman.annenkov@gmail.com>
2024-03-28 13:25:44 +0300

    added project for hw from seminar seven

---
+++ hwSeminarSeven/src/main/java/com/example/hwSeminarSeven/HwSeminarSevenApplication.java
@@ -0,0 +1,13 @@
+package com.example.hwSeminarSeven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSeminarSevenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwSeminarSevenApplication.class, args);
+	}
+
+}
---
+++ hwSeminarSeven/src/test/java/com/example/hwSeminarSeven/HwSeminarSevenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwSeminarSeven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSeminarSevenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
