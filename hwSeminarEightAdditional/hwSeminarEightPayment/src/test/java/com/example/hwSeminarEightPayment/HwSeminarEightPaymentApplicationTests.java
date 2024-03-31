94fa160310cbda63b30a2ef98dc1fca1bccd19ea
Roman Annenkov <roman.annenkov@gmail.com>
2024-03-31 14:07:18 +0300

    added three project for additional task from seminar eight hw

---
+++ hwSeminarEightAdditional/hwSeminarEightPayment/src/test/java/com/example/hwSeminarEightPayment/HwSeminarEightPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwSeminarEightPayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSeminarEightPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ hwSeminarEightAdditional/hwSeminarEightStore/src/main/java/com/example/hwSeminarEightStore/HwSeminarEightStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.hwSeminarEightStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSeminarEightStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwSeminarEightStoreApplication.class, args);
+	}
+
+}
